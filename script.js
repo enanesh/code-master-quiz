bc56228646736b2b831e22cceb8e2e9439ffa771
enanesh <edna.ganesha.gr@gmail.com>
2022-10-20 01:06:37 -0700

    Pseudo code

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 //DOM HOOKS 
 
+var time = document.getElementById("timer");//has to keep track of this
+var scoreBoard = 0;// Have to keep track of this
+var questions = 0;//has to be an  objet 
+var answers = 0;// has to be random sort choice.Validate 
 
-//var time = document.getElementById("grid-item2");
+//add event listener to startButton
+//Present a question
+// if answerwrong  (-) time,next question
+//if answer is right save answer ,next question
+//when timer =0 finish
+//at the end of the game  user iinput initial and show score sorted
+
+
+
+
+
+
+
+
 
 
 

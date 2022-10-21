1ef2838b748d89d38e09b133263bd6b3d48315a4
enanesh <edna.ganesha.gr@gmail.com>
2022-10-21 01:26:08 -0700

    Random array

--- script.js
+++ script.js
@@ -79,7 +79,43 @@ var questionLibrary = [
 ];
 
 
-// SHUFFLE ARRAY TO GENERATE RANDOM QUESTION
+// SHUFFLE ARRAY TO GENERATE RANDOM QUESTION (got this part from stack-overflow)
+
+
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+};
+
+
+shuffle(questionLibrary);
+
+console.log(questionLibrary);
+
+//asignar mi pregunta
+//crear bottones para la respuesta
+//tratar de iterar las respuestas 
+
+
+
+
+
+
+
+
 
 
 
